e7be19606f2b01c93425dd136ccd8656fd6a919d
Nara-Bayaraa <nara.sdet@gmail.com>
2025-05-19 15:42:28 -0500

    modify baseUrl

--- cypress.config.js
+++ cypress.config.js
@@ -4,7 +4,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: "https://www.saucedemo.com/",
+    baseUrl: "https://www.saucedemo.com/v1",
     env: {
       standardUsername: process.env.STANDARD_USER_USERNAME,
       standardPassword: process.env.STANDARD_USER_PASSWORD,
